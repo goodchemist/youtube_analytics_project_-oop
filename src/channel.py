8d7357f2730b16ceac25aa8734bbb2b97f6ebf8c
good_chemist <iolgasergeevna@yandex.ru>
2023-12-14 18:02:48 +0800

    add __gt__ method

--- src/channel.py
+++ src/channel.py
@@ -45,6 +45,14 @@ class Channel:
         """
         return int(self.subscriber_count) - int(other.subscriber_count)
 
+    def __gt__(self, other):
+        """
+        Метод для операции сравнения "больше".
+        :param other: экзепляр класса Channel
+        :return: True или False
+        """
+        return int(self.subscriber_count) > int(other.subscriber_count)
+
     def get_info_channel(self):
         """
         Метод, возвращающий информацию о канале
