6d5694adc8a611b970dec05590e2b6fbc0df4d6d
good_chemist <iolgasergeevna@yandex.ru>
2023-12-27 17:56:09 +0800

    add method 'total_duration' in class 'PlayList'

--- src/playlist.py
+++ src/playlist.py
@@ -9,7 +9,6 @@ api_key: str = os.getenv('YT_API_KEY')
 class PlayList:
 
     def __init__(self, playlist_id: str) -> None:
-
         self.__playlist_id = playlist_id  # id плейлиста
 
         self.url = f'https://www.youtube.com/playlist?list={self.__playlist_id}'  # ссылкa на плейлист
@@ -35,3 +34,67 @@ class PlayList:
         youtube = self.get_service()
         playlist = youtube.playlists().list(id=self.__playlist_id, part='snippet').execute()
         return playlist
+
+    def get_video_response(self, video_ids):
+        """
+        Метод, возвращающий информацию о видео из плейлиста по их id.
+        :param video_ids: список всех id видео из плейлиста
+        :return: словарь с информацией о видео из плейлиста по их id.
+        """
+        youtube = self.get_service()
+        video_response = youtube.videos().list(part='contentDetails,statistics',
+                                               id=','.join(video_ids)
+                                               ).execute()
+        return video_response
+
+    def get_info_about_playlist_videos(self):
+        """
+        Метод, возвращающий информацию об видео из плейлиста.
+        :return: словарь с информацией об видео из плейлиста.
+        """
+        youtube = self.get_service()
+        playlist_videos = youtube.playlistItems().list(playlistId=self.__playlist_id,
+                                                       part='contentDetails',
+                                                       maxResults=50,
+                                                       ).execute()
+        return playlist_videos
+
+    @staticmethod
+    def get_video_ids(playlist_videos) -> list:
+        """
+        Метод, возвращающий список всех id видео из плейлиста.
+        :param playlist_videos: словарь с информацией об видео из плейлиста
+        :return: список всех id видео из плейлиста.
+        """
+        video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
+        return video_ids
+
+    @property
+    def total_duration(self) -> datetime.timedelta:
+        """
+        Возвращает суммарную длительность плейлиста.
+
+        :return: объект класса `datetime.timedelta`
+        """
+
+        playlist_videos = self.get_info_about_playlist_videos()
+
+        video_ids = self.get_video_ids(playlist_videos)
+
+        video_response = self.get_video_response(video_ids)
+
+        total_duration_ = datetime.timedelta()
+
+        for video in video_response['items']:
+            # YouTube video duration is in ISO 8601 format
+            iso_8601_duration = video['contentDetails']['duration']
+            duration = isodate.parse_duration(iso_8601_duration)
+
+            # Get datetime.timedelta object
+            hours, minutes, seconds = str(duration).split(':')
+            duration_timedelta = datetime.timedelta(hours=int(hours), minutes=int(minutes), seconds=int(seconds))
+
+            # Calculate the total time
+            total_duration_ += duration_timedelta
+
+        return total_duration_
